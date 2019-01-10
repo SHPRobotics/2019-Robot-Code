8efca78e08f70e74f1ea809f46be711b78379abe
Seton Hall Prep Robotics <setonhallrobotics@gmail.com>
2019-01-10 16:58:11 -0500

    Update PixyPacket.java

--- PixyPacket.java
+++ PixyPacket.java
@@ -1,9 +1,9 @@
 package frc.robot;
 
 public class PixyPacket {
-    public int X;
-    public int Y;
-    public int Width;
-    public int Height;
+    public int x;
+    public int y;
+    public int width;
+    public int height;
     }
     
